f06a3d509b26e51f0b1263f9f6f0ac463e6de4ca
Pedro Ramos <pedrotr@hotmail.com>
2024-07-02 14:25:19 +0100

    config: updated tailwind and liting files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,15 +8,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      // backgroundImage: {
-      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      //   "gradient-conic":
-      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      // },
-      gridTemplateColumns: {
-        // Extends the default grid column settings
-        15: "repeat(15, minmax(0, 1fr))",
-        20: "repeat(20, minmax(0, 1fr))",
+      colors: {
+        dark: "#090908",
+        beige: "#EEE9DB",
+        orangeDark: "#783D19",
+        orangeLight: "#C4661F",
+        greyCustom: "#555555",
+        yellow: "#F9EBC7",
+        greenLight: "#A9B388",
+        greenDark: "#5F6F52",
+      },
+      cursor: {
+        beige: "url('/images/cursor.svg'), auto", // Ensure the path is correct
+      },
+      keyframes: {
+        "ig-stroke": {
+          "0%": { stroke: "#8a3ab8", strokeDasharray: "1" },
+          "100%": { stroke: "#cd486b", strokeDasharray: "100, 0" },
+        },
+      },
+      animation: {
+        "ig-stroke": "ig-stroke 15s ease-out alternate 3s",
       },
     },
   },
