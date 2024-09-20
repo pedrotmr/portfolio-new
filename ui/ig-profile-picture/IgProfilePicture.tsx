b1a207c04b945e57f04e9cf351ba9da5e639ff11
Pedro Ramos <pedrotr@hotmail.com>
2024-09-20 12:12:06 -0300

    chore: prettier on IgProfilePicture

--- ui/ig-profile-picture/IgProfilePicture.tsx
+++ ui/ig-profile-picture/IgProfilePicture.tsx
@@ -11,13 +11,7 @@ type IgProfilePictureProps = {
   transition?: AnimationProps['transition'];
 };
 
-const IgProfilePicture = ({
-  className,
-  style,
-  initial,
-  animate,
-  transition,
-}: IgProfilePictureProps) => {
+const IgProfilePicture = ({ className, style, initial, animate, transition }: IgProfilePictureProps) => {
   return (
     <motion.div
       className={cn('relative z-20 w-max animate-ig-stroke', className)}
@@ -26,20 +20,8 @@ const IgProfilePicture = ({
       animate={animate}
       transition={transition}
     >
-      <svg
-        width="74"
-        height="74"
-        viewBox="0 0 74 74"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <circle
-          cx="37"
-          cy="37"
-          r="34"
-          stroke="url(#paint0_linear_658_19773)"
-          strokeWidth="2.5"
-        ></circle>
+      <svg width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="37" cy="37" r="34" stroke="url(#paint0_linear_658_19773)" strokeWidth="2.5"></circle>
         <defs>
           <linearGradient
             id="paint0_linear_658_19773"
@@ -54,14 +36,7 @@ const IgProfilePicture = ({
             <stop offset="1" stopColor="#8134AF"></stop>
           </linearGradient>
         </defs>
-        <image
-          x="7"
-          y="7"
-          width="60"
-          height="60"
-          href="/images/perfil_3.jpg"
-          clipPath="circle(30)"
-        />
+        <image x="7" y="7" width="60" height="60" href="/images/perfil_3.jpg" clipPath="circle(30)" />
       </svg>
     </motion.div>
   );
