a316d6fce7dbe130b8320f4271771dda13052bd3
Pedro Ramos <pedrotr@hotmail.com>
2024-09-20 13:29:53 -0300

    style: linted use track cursor

--- hooks/use-track-cursor.ts
+++ hooks/use-track-cursor.ts
@@ -1,10 +1,11 @@
 import { useMotionValue } from 'framer-motion';
+import { MouseEvent } from 'react';
 
 const useTrackCursor = () => {
   const mouseX = useMotionValue(0);
   const mouseY = useMotionValue(0);
 
-  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+  const handleMouseMove = (event: MouseEvent<HTMLDivElement>) => {
     mouseX.set(event.clientX);
     mouseY.set(event.clientY);
   };
