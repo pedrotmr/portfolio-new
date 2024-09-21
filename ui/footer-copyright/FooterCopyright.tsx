3a8732fb1c1b2bf5644008123525fb75aebb2a2d
Pedro Ramos <pedrotr@hotmail.com>
2024-09-21 17:37:16 -0300

    feat: footer copyright component

--- ui/footer-copyright/FooterCopyright.tsx
+++ ui/footer-copyright/FooterCopyright.tsx
@@ -2,7 +2,7 @@ const FooterCopyright = () => {
   return (
     <footer className="bg-slate-900 py-3 text-center">
       <p className="font-mono text-sm text-beige">
-        © {new Date().getFullYear()} | Crafted with by Pedro Ramos.
+        © {new Date().getFullYear()} | Crafted by Pedro Ramos with Next.js, Tailwind and Framer Motion.
       </p>
     </footer>
   );
