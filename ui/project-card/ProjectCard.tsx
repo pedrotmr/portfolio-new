fc33f028b95f82e38c2c08b344349290db9ee18e
Pedro Ramos <pedrotr@hotmail.com>
2024-09-30 15:18:20 -0300

    feat: disable some animations from projects cards

--- ui/project-card/ProjectCard.tsx
+++ ui/project-card/ProjectCard.tsx
@@ -1,6 +1,6 @@
 import { GradientText, TagList } from '@/ui';
 import { avantGarde } from '@/utils';
-import { motion, MotionValue, useScroll, useTransform } from 'framer-motion';
+import { motion, MotionValue, useTransform } from 'framer-motion';
 import { ArrowUpRight } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -18,20 +18,20 @@ type ProjectCardProps = {
 const ProjectCard = ({ index, project, containerProgress, range, targetScale }: ProjectCardProps) => {
   const cardRef = useRef(null);
 
-  const { scrollYProgress } = useScroll({
-    target: cardRef,
-    offset: ['start end', 'start 0.3'],
-  });
+  // const { scrollYProgress } = useScroll({
+  //   target: cardRef,
+  //   offset: ['start end', 'start 0.3'],
+  // });
 
   const cardScale = useTransform(containerProgress, range, [1, targetScale]);
-  const imageScale = useTransform(scrollYProgress, [0, 1], [1.6, 1]);
+  // const imageScale = useTransform(scrollYProgress, [0, 1], [1.6, 1]);
 
   return (
     <motion.div
       ref={cardRef}
       className="card-wrapper relative h-max w-full overflow-hidden rounded-3xl bg-gradient-to-b from-slate-700 to-slate-800
         p-[1px] backdrop-blur-md transition-shadow will-change-transform before:absolute before:left-0 before:top-0
-        before:h-full before:w-full before:animate-border-spin before:cursor-none before:content-['']"
+        before:h-full before:w-full before:cursor-none before:content-['']"
       style={
         {
           scale: cardScale,
@@ -54,7 +54,7 @@ const ProjectCard = ({ index, project, containerProgress, range, targetScale }: 
               ))}
             </div>
             <Link
-              href={project.github}
+              href={project.link || project.github}
               target="_blank"
               rel="noopener noreferrer"
               className="group flex items-end gap-1 underline underline-offset-4"
@@ -68,15 +68,15 @@ const ProjectCard = ({ index, project, containerProgress, range, targetScale }: 
             <TagList tags={project.tags} />
           </div>
           <div className="relative h-max w-full self-center overflow-hidden rounded-3xl">
-            <motion.div style={{ scale: imageScale }} className="h-full w-full">
-              <Image
-                src={project.image}
-                alt={project.name}
-                className="aspect-[3/2] h-auto w-full object-cover"
-                height={1000}
-                width={1000}
-              />
-            </motion.div>
+            {/* <motion.div style={{ scale: imageScale }} className="h-full w-full"> */}
+            <Image
+              src={project.image}
+              alt={project.name}
+              className="aspect-[3/2] h-auto w-full object-cover"
+              height={1000}
+              width={1000}
+            />
+            {/* </motion.div> */}
           </div>
         </div>
       </div>
