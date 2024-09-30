950e688d23386f662ae0545e7454a44f8eff820b
Pedro Ramos <pedrotr@hotmail.com>
2024-09-30 15:18:52 -0300

    chore: update projects data

--- ui/projects/project-data.ts
+++ ui/projects/project-data.ts
@@ -16,18 +16,18 @@ export const projects: Project[] = [
       'The app was created with the idea that everyone could enjoy a good cocktail at home, without having to go out and spend lots of money.',
     ],
     tags: ['TypeScript', 'React', 'Saas', 'Redux', 'Cypress', 'Node.js', 'Express', 'MongoDB'],
-    image: '/images/projects/juicy.gif',
+    image: '/images/projects/juicy.png',
   },
-  {
-    name: 'Schlepper',
-    github: 'https://github.com/pedrotmr/schlepper-app',
-    description: [
-      'Schlepper is a record/tape/cd trading application that focuses on in-town trades to bypass the growing expense of the post-office.',
-      'Users can add albums to their collection and make trades with people in thier community by finding them in the "find trades" section.',
-    ],
-    tags: ['TypeScript', 'React', 'Redux', 'Node.js', 'Express', 'PostgresSQL'],
-    image: '/images/projects/schlepper.gif',
-  },
+  // {
+  //   name: 'Schlepper',
+  //   github: 'https://github.com/pedrotmr/schlepper-app',
+  //   description: [
+  //     'Schlepper is a record/tape/cd trading application that focuses on in-town trades to bypass the growing expense of the post-office.',
+  //     'Users can add albums to their collection and make trades with people in thier community by finding them in the "find trades" section.',
+  //   ],
+  //   tags: ['TypeScript', 'React', 'Redux', 'Node.js', 'Express', 'PostgresSQL'],
+  //   image: '/images/projects/schlepper.gif',
+  // },
   {
     name: 'Zims',
     github: 'https://github.com/pedrotmr/zims',
@@ -38,17 +38,17 @@ export const projects: Project[] = [
     tags: ['JavaScript', 'React', 'Redux', 'Chakra UI', 'Truffle', 'Ganache', 'Moralis'],
     image: '/images/projects/zims.gif',
   },
-  // {
-  //   name: 'News Aggregator',
-  //   github: 'https://github.com/pedrotmr/news-aggregator',
-  //   link: 'https://news-aggregator-kohl-three.vercel.app/',
-  //   description: [
-  //     'This website aggregates news articles from different sources, offering a streamlined and personalized way to stay informed.',
-  //     'Users can customize their news feed based on personal preferences, ensuring relevant and tailored content.',
-  //   ],
-  //   tags: ['JavaScript', 'React', 'React Query', 'TailwindCSS', 'Docker'],
-  //   image: '/images/projects/news-aggregator.png',
-  // },
+  {
+    name: 'News Aggregator',
+    github: 'https://github.com/pedrotmr/news-aggregator',
+    link: 'https://news-aggregator-kohl-three.vercel.app/',
+    description: [
+      'This website aggregates news articles from different sources, offering a streamlined and personalized way to stay informed.',
+      'Users can customize their news feed based on personal preferences, ensuring relevant and tailored content.',
+    ],
+    tags: ['JavaScript', 'React', 'React Query', 'TailwindCSS', 'Docker'],
+    image: '/images/projects/news-aggregator.png',
+  },
   {
     name: 'Crypto Tools',
     github: 'https://github.com/pedrotmr/crypto-tools',
@@ -59,5 +59,16 @@ export const projects: Project[] = [
     ],
     tags: ['JavaScript', 'React', 'TailwindCSS', 'SWR', 'Ethers.js', 'Lightweight Charts'],
     image: '/images/projects/crypto-tools.png',
+  },
+  {
+    name: 'React Shortcut Keys',
+    github: 'https://github.com/pedrotmr/react-shortcut-keys',
+    link: 'https://www.npmjs.com/package/react-shortcut-keys',
+    description: [
+      'This package provides a way to handle keyboard shortcuts in a React application.',
+      'It includes a custom hook and a component to facilitate the handling of keyboard shortcuts, with support for case sensitivity and additional customization options.',
+    ],
+    tags: ['TypeScript', 'React', 'React Testing Library', 'Changesets', 'Github Actions'],
+    image: '/images/projects/react-shortcut-keys.png',
   },
 ];
